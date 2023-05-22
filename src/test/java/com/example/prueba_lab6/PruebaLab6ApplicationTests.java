1d74db14bf57e16d8a5e5aa90b12fc4a69d8d3c4
Ana Sofia <alejandro.as@pucp.edu.pe>
2023-05-22 13:55:44 -0500

    Initial commit_1

---
+++ src/test/java/com/example/prueba_lab6/PruebaLab6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prueba_lab6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaLab6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
